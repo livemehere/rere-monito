3fa45352dc267cc5c0c22a9a927d105b7cfab825
kongtaemin <rhdxoals@gmail.com>
2022-04-05 17:07:39 +0900

    login 완료

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { userState } from "../atoms/user";
 
 export default function Home() {
-  return <div>Home</div>;
+  const [user, setUser] = useRecoilState(userState);
+
+  useEffect(() => {
+    console.log("로그인정보", user);
+  }, [user]);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/login">Login</Link>
+    </div>
+  );
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { userState } from "../atoms/user";
+import axiosManager from "../util/axiosManager";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [user, setUser] = useRecoilState(userState);
+  const navigate = useNavigate();
+
+  const handleLogin = async (email, password) => {
+    try {
+      const result = await axiosManager.axios("/signIn", "POST", {
+        email,
+        password,
+      });
+
+      const userData = await axiosManager.axios("/signIn/verify", "POST", {
+        token: result,
+      });
+      setUser(userData);
+      navigate("/");
+    } catch (e) {
+      alert("로그인에 실패하였습니다");
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    handleLogin(email, password);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <Link to="/">HOME</Link>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="email" name="email" />
+        <input type="text" placeholder="password" name="password" />
+        <button>로그인</button>
+      </form>
+    </div>
+  );
 }
