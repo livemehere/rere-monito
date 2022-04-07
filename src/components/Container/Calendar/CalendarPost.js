73ae27b3aabd06b2fde2070df258e57feb078730
euion <qq212420@gmail.com>
2022-04-07 22:42:23 +0900

    nav적용

--- src/components/Container/Calendar/CalendarPost.js
+++ src/components/Container/Calendar/CalendarPost.js
@@ -3,6 +3,5 @@ import axios from "axios";
 export function getEvent(postId) {
     return axios.get(
         { id: 1, title: '시험일', date: '2022-03-28', detail:"사회관 302호"},
-        { id: 2, title: '캡스톤 회의', date: '2022-03-20',detail:"공학관 502호" }
     )
 }
