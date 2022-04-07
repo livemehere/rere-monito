d1d55a837f5e8300369bd6e3eb058b684605a078
euion <qq212420@gmail.com>
2022-04-07 18:11:47 +0900

    캘린더빌드오류수정(다시)

--- src/components/Container/Calendar/CalendarBody.js
+++ src/components/Container/Calendar/CalendarBody.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import axios from 'axios';
 import { createSelector } from 'reselect';
 import FullCalendar, { formatDate } from '@fullcalendar/react';
