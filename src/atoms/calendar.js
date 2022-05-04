93bd9b49849a4b3d2056c51aeafce4dc317fccf6
euion <qq212420@gmail.com>
2022-05-04 10:32:06 +0900

    calendar 상태관리 병합

--- src/atoms/calendar.js
+++ src/atoms/calendar.js
@@ -2,5 +2,11 @@ import { atom } from "recoil";
 
 export const calendarState = atom({
   key: "calendar",
-  default: null,
+  default: [{
+    id: 14,
+    title: "캡스톤회의",
+    start: "2022-05-27",
+    end: "2022-05-28",
+    // detail:"메모부터하자!"
+  }]
 });
--- src/components/Container/Calendar/CountEventstate.js
+++ src/components/Container/Calendar/CountEventstate.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { userState } from "../../../atoms/user";
 import { CountingEvent } from "../../Presenter/Calendar/CountingEventPresenter";
-import { User } from "./CalendarBody";
+
+ function User() {
+  const [user, setUser] = useRecoilState(userState);
+   return `${user.name}`
+};
 
 const CountingEventState = ({ data }) => {
   return (
@@ -8,7 +14,7 @@ const CountingEventState = ({ data }) => {
       <CountingEvent>
         <div className="demo-app-sidebar">
           <div className="demo-app-sidebar-section">
-            <h2>{User.name}님의 일정입니다.</h2>
+            <h2>{User()}님의 일정입니다.</h2>
             <ul className="notice-container">
               <li className="notice-list">날짜를 선택하면 새 일정을 생성하라는 메시지가 표시됩니다.</li>
               <li className="notice-list">일정 드래그, 드롭 및 크기 조정이 가능합니다</li>
@@ -26,7 +32,7 @@ const CountingEventState = ({ data }) => {
             </label>
           </div>
           <div className="demo-app-sidebar-section">
-            <h2>{User.name}님의 일정갯수 : </h2>
+            <h2>{User()}님의 이번달 일정갯수 : </h2>
             {data && data.map((d) => <div key={d.id}>{d.title}</div>)}
             <ul>{/* {this.props.events.map(renderSidebarEvent)} */}</ul>
           </div>
