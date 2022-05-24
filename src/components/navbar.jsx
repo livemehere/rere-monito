258bcf6c12f52a1d627d52fce99cedc29a0498b3
0801euiyeon <qq212420@gmail.com>
2022-05-25 01:23:59 +0900

    edit toastify in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,16 +11,8 @@ import { loginState } from "../atoms/loginState";
 import {toast, ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
+
 export default function NavBar() {
-  const info = toast.info("🦄 저번주 대비 학습량이 늘었습니다.", {
-    autoClose:5000,
-    position: toast.POSITION.TOP_RIGHT,
-    hideProgressBar: false,
-    closeOnClick: false,
-    pauseOnHover: true,
-    pauseOnFocusLoss: false,
-    draggable: true,
-    });
 
   const [isLoggedIn, setIsLoggedIn] = useRecoilState(loginState);
   const logoutHandler = () => {
