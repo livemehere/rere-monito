56e59e125a3823b6d7722e65afd68a0af8f426a9
kongtaemin <rhdxoals@gmail.com>
2022-06-07 14:54:42 +0900

    ssl 인증 후

--- src/util/axiosManager.js
+++ src/util/axiosManager.js
@@ -4,13 +4,13 @@ class AxiosManager {
   async axios(url, method, body) {
     const data = await axios({
       method,
-      url: `http://15.164.167.169:3000${url}`,
+      url: `https://monito.ml${url}`,
       data: body && body,
     });
 
-    if(data.status >= 200 && data.status <= 299){
+    if (data.status >= 200 && data.status <= 299) {
       return data.data;
-    }else{
+    } else {
       return data;
     }
   }
